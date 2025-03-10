13720032abb00b60b5ae9b885454944b34a42387
DE-Uehara <r.uehara@defide-ix.com>
2025-03-10 18:02:06 +0900

    practice1

---
+++ pages/posts/[id].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  validate: async (route) => {
+    // Check if the id is made up of digits
+    return typeof route.params.id === 'string' && /^\d+$/.test(route.params.id)
+  }
+})
+</script>
